df792b7af59cc873b62690504ce90c099d42cb1e
PascalUlor <pascalulor@yahoo.com>
2018-10-14 15:02:55 +0100

    (feature): create an inventory page display inventory in a table add side navigation drawer for mobileview [Delivers #161109133]

---
+++ UI/js/main.js
@@ -0,0 +1,27 @@
+const backdrop = document.querySelector('.backdrop');
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
+
+
+const backDrop = () => {
+  backdrop.classList.remove('open');
+};
+const mobileButton = () => {
+  mobileNav.classList.add('open');
+  backdrop.classList.add('open');
+};
+const removeBackDrop = () => {
+  mobileNav.classList.remove('open');
+  backDrop();
+};
+
+const animate = () => {
+  toggleButton.addEventListener('click', mobileButton);
+  backdrop.addEventListener('click', removeBackDrop);
+};
+
+animate().then(() => {
+  backdrop.removeEventListener('click', removeBackDrop);
+  toggleButton.removeEventListener('click', mobileButton);
+});
+

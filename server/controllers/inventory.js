f017353be1c60e4efb4ef507902dd90124e1f837
PascalUlor <pascalulor@yahoo.com>
2018-10-22 02:10:59 +0100

    (feature): set up REST api for admin to create products in the inventory Admin can add products when he or she hits this route [Delivers #161304042]

---
+++ server/controllers/inventory.js
@@ -0,0 +1,39 @@
+import db from '../models/testData';
+
+/**
+ * Class for /api/routes
+ * @class inventoryController
+ */
+export default class inventoryController {
+  /**
+     * API method to (POST) create a product
+     * @param {obj} req
+     * @param {obj} res
+     * @returns {obj} insertion error messages or success messages
+     */
+  static createProduct(req, res) {
+    if (parseInt(req.body.userId, 10) === db.userDataBase[0].id) {
+      db.inventory.push({
+        id: db.inventory.length + 1,
+        userId: parseInt(req.body.userId, 10),
+        productName: req.body.productName,
+        productCategory: req.body.productCategory,
+        description: req.body.description,
+        status: req.body.status,
+        dateAdded: req.body.dateAdded,
+      });
+      res.status(201);
+      res.json({
+        success: true,
+        message: 'Request created successfully',
+        data: db.inventory[db.inventory.length - 1],
+      });
+    } else {
+      res.status(400);
+      res.json({
+        success: false,
+        message: 'You are not authorized to create products',
+      });
+    }
+  }// Method to create products ends
+}
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import inventoryController from '../controllers/inventory';
 
 const router = express.Router();
+
+
+router.route('/products')
+  .post(inventoryController.createProduct);
 
 
 export default router;
